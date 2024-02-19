ce6e6ea8470c17f7c79ce0ec01850a14a6780464
uls19 <uls19@mail.ru>
2024-02-19 23:27:15 +0200

    form_commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//загрузка
+// загрузка
 const mask = document.querySelector('.mask');
 window.addEventListener('load', () => {
   mask.classList.add('hide');
@@ -26,3 +26,41 @@ mask.remove();
       clickable: true,
     },
   });
+
+// Считываем поле ввода
+const phoneInput = document.querySelector(".phone");
+// Считываем кнопку
+const btn = document.querySelector(".btn");
+
+// Создаем маску в инпуте
+const phoneMask = new IMask(phoneInput, {
+  mask: "+{7}(000)000-00-00",
+});
+
+// Обработчик события для инпута
+phoneInput.addEventListener("input", phoneInputHandler);
+// Обработчик события для кнопки
+btn.addEventListener("click", btnHandler);
+
+// Если ввели правлильно - кнопка активна
+function phoneInputHandler() {
+  if (phoneMask.masked.isComplete) {
+    btn.classList.add("btn_active");
+  } else {
+    btn.classList.remove("btn_active");
+  }
+}
+
+btn.addEventListener ("click", btnHandler);
+// Отправляем номер телефона
+  async function btnHandler(e) {
+    e.preventDefault();
+    console.log(phoneMask.masked.unmaskedValue);
+    // return await fetch("send_msg.php", {
+    //   method: "POST",
+    //   body: phoneMask.unmaskedValue,
+    // });
+  }
+ 
+
+
